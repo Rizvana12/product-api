3c236a588c4568d44c50636f2990ec4cc23568fd
Rizvana12 <kmrizvana3@gmail.com>
2025-12-14 19:10:30 +0530

    first backend project

---
+++ seed.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Product = require("./models/product");
+
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB connected"));
+
+const products = [
+  { name: "Laptop", price: 55000, category: "Electronics", image:"https://res.cloudinary.com/dquadclwl/image/upload/v1765707217/computer-820281_1280_a00hu1.jpg" },
+  { name: "Mobile Phone", price: 30000, category: "Mobiles", image:"https://res.cloudinary.com/dquadclwl/image/upload/v1765707235/iphone-410324_1280_ts8yd5.jpg" },
+  { name: "Headphones", price: 2000, category: "Accessories", image:"https://res.cloudinary.com/dquadclwl/image/upload/v1765707253/audio-1840073_1280_c5yqla.jpg" },
+  { name: "Keyboard", price: 1500, category: "Accessories", image:"https://res.cloudinary.com/dquadclwl/image/upload/v1765707274/keyboard-1628579_1280_pzisfl.jpg" },
+  { name: "Tablet", price: 25000, category: "Electronics", image:"https://res.cloudinary.com/dquadclwl/image/upload/v1765707295/technology-792180_1280_ggo15a.jpg" },
+  { name: "Smart Watch", price: 8000, category: "Wearables", image:"https://res.cloudinary.com/dquadclwl/image/upload/v1765708050/watch-6871817_1280_gft1kl.jpg" }
+];
+
+async function seedProducts() {
+  try {
+    await Product.deleteMany();       // avoid duplicates
+    await Product.insertMany(products);
+    console.log("Products seeded successfully");
+    process.exit();
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+seedProducts();
---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product");
+
+require('dotenv').config();
+
+const app = express();
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("MongoDB connection error:", err));
+
+
+app.get("/api/products", async (req, res) => {
+  try {
+    // Pagination
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 5;
+    const skip = (page - 1) * limit;
+
+    // Filters
+    const { category, search } = req.query;
+    const filter = {};
+
+    if (category) {
+      filter.category = category;
+    }
+
+    if (search) {
+      filter.name = { $regex: search, $options: "i" };
+    }
+
+    const totalProducts = await Product.countDocuments(filter);
+
+    const products = await Product.find(filter)
+      .skip(skip)
+      .limit(limit)
+      .lean();
+
+    res.json({
+      page,
+      limit,
+      totalProducts,
+      totalPages: Math.ceil(totalProducts / limit),
+      products
+    });
+
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Server running on port 3000");
+});
